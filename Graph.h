f073c6b1ca127746e63844d2ee93aab82c109eb7
Eli Freid <86243882+E-Freid@users.noreply.github.com>
2023-04-03 17:01:39 +0300

    implemented h file for Graph like in we did in figma

--- Graph.h
+++ Graph.h
@@ -1,13 +1,32 @@
-//
-// Created by Noel Freid on 03/04/2023.
-//
 
 #ifndef PROGPROJ_GRAPH_H
 #define PROGPROJ_GRAPH_H
+using namespace std;
+
+#include <list>
+
+typedef struct edge
+{
+    int from;
+    int to;
+    bool visited;
+} Edge;
 
 
-class Graph {
-    
+class Graph
+{
+private:
+    int m_NumOfNodes;
+    list<Edge>* m_adjList;
+
+    Graph(int i_NumOfNodes);
+
+public:
+    virtual void addEdges(Edge* i_edgesList, int i_numOfEdges) = 0;
+    virtual bool checkIfGraphEulerian() = 0;
+    virtual void printEularGraphIfExists() = 0; // TODO: avi should decide if to keep method here, or in sons
+    virtual ~Graph();
+
 };
 
 
